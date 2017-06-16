5358fc90185e9fc7df7a949c6c2f4fcf02595f02
kristjanjansen <kristjan.jansen@gmail.com>
2017-06-16 11:54:37 +0300

    fixes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fallback = require('express-history-api-fallback')
 var MongoOplog = require('mongo-oplog')
-const oplog = MongoOplog('mongodb://127.0.0.1:27017/local', { ns: 'test.documents' })
 
 app.use(express.static(__dirname))
 app.use(fallback('index.html', { root: __dirname }))
 
+var oplog = MongoOplog('mongodb://127.0.0.1:27017/local', { ns: 'test.documents' })
 oplog.tail();
 
 io.on('connection', function (socket) {
